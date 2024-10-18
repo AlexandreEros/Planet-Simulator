5245caa1df5ab637701d507381e71ccda2c14929
Alexandre Eros Silva <alexandrees1996@gmail.com>
2024-10-17 23:05:45 -0300

    Celestial bodies can now only be moved by setting a net force with the method 'apply_force', and then calling the methods 'accelerate' and 'move' with specific time steps.

--- celestial_body.py
+++ celestial_body.py
@@ -1,19 +1,31 @@
 import numpy as np
 
 class CelestialBody:
-    def __init__(self, name: str, position: np.ndarray[np.float64], velocity: np.ndarray[np.float64], mass: float,
+    def __init__(self, name: str, position: np.ndarray, velocity: np.ndarray, mass: float,
                  color: str, orbital_period: float = None, eccentricity: float = 0.0, year_percentage: float = 0.0):
         self.name = name
-        self.position: np.ndarray[np.float64] = position
-        self.velocity: np.ndarray[np.float64] = velocity
+        self.position = position
+        self.velocity = velocity
         self.mass = mass
         self.color = color
         self.orbital_period = orbital_period
         self.eccentricity = eccentricity
         self.initial_year_percentage = year_percentage
 
-    def accelerate(self, delta_v: np.ndarray[np.float64]) -> None:
-        self.velocity += delta_v
+        self.net_force = np.zeros((3,), dtype = np.float64)
 
-    def move(self, delta_x: np.ndarray[np.float64]) -> None:
-        self.position += delta_x
+
+    def apply_force(self, force):
+        self.net_force = force
+
+    # def accelerate(self, delta_v: np.ndarray) -> None:
+    #     self.velocity += delta_v
+    def accelerate(self, delta_t):
+        self.velocity += self.net_force * delta_t / self.mass
+
+    def move(self, delta_t) -> None:
+        self.position += self.velocity * delta_t
+
+    @property
+    def current_angular_momentum(self):
+        return np.cross(self.position, self.mass * self.velocity)
